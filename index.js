2e9a43e307f974a9693ab4d37b21bf4b2ea01335
kasra <kasramohamadpor@yahoo.com>
2021-12-16 20:41:44 +0330

    add codes

--- index.js
+++
@@ -1,5 +0,0 @@
-const emojiList = ['🤗', '🍀', '(⚈ ̫ ⚈)', '😀', '😁', '😅', '😇', '😉'];
-const p = document.querySelector('p');
-p.addEventListener('click', () => {
-  p.innerText = emojiList[Math.floor((Math.random() * 100) % emojiList.length)];
-});
---
+++ instagram.js
@@ -0,0 +1,29 @@
+const backward = document.querySelector(".leftSideLogo");
+backward.addEventListener("click", () => {
+  console.log("Backward");
+});
+
+const setting = document.querySelector(".rightSideLogo");
+setting.addEventListener("click", () => {
+  console.log("Setting");
+});
+
+const link = document.querySelector("#link");
+link.addEventListener("click", () => {
+  console.log("Link");
+});
+
+const follow = document.querySelector("#follow");
+follow.addEventListener("click", () => {
+  console.log("Follow");
+});
+
+const message = document.querySelector("#message");
+message.addEventListener("click", () => {
+  console.log("Message");
+});
+
+const suggestion = document.querySelector("#suggestion");
+suggestion.addEventListener("click", () => {
+  console.log("Suggestion");
+});
