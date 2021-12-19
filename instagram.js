69a86d7b99ce82e62a9a88bc9ea5c43249409317
kasra <kasramohamadpor@yahoo.com>
2021-12-19 17:38:37 +0330

    change js file

--- instagram.js
+++ instagram.js
@@ -1,29 +1,13 @@
-const backward = document.querySelector(".leftSideLogo");
-backward.addEventListener("click", () => {
-  console.log("Backward");
-});
-
-const setting = document.querySelector(".rightSideLogo");
-setting.addEventListener("click", () => {
-  console.log("Setting");
-});
-
-const link = document.querySelector("#link");
-link.addEventListener("click", () => {
-  console.log("Link");
-});
-
-const follow = document.querySelector("#follow");
-follow.addEventListener("click", () => {
-  console.log("Follow");
-});
-
-const message = document.querySelector("#message");
-message.addEventListener("click", () => {
-  console.log("Message");
-});
-
-const suggestion = document.querySelector(".smallButton");
-suggestion.addEventListener("click", () => {
-  console.log("Suggestion");
-});
+const buttons = document.querySelectorAll(".btn");
+const clicked = () => {
+  buttons.forEach((item) => {
+    item.addEventListener("click", () => {
+      console.log(item.classList[0]);
+    });
+  });
+};
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", clicked);
+} else {
+  clicked();
+}
