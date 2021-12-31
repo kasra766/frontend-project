0fbf5c031d45f487f4120b371c74a3fbbf5e0e09
kasra <kasramohamadpor@yahoo.com>
2021-12-31 18:16:31 +0330

    some change

--- react.js
+++ react.js
@@ -12,12 +12,30 @@
 const App = () => {
   const { useState, useEffect } = React;
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+
+  
   useEffect(() => {
-    fetch("https://random-d.uk/api/v2")
+    fetch("http://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((json) => setData(json));
+      .then((res) => {
+        setData(res);
+        setLoading(false);
+      }),
+      (error) => {
+        console.error("Error fetching data:", error);
+      };
   }, [data]);
-  return <div>{JSON.stringify(data)}</div>;
+
+
+
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
+    <div>{JSON.stringify(data, null, 2)}</div>
+  );
 };
 //////////////////////////////////////////////////////
 ReactDOM.render(<App />, document.getElementById("root"));
